f523708ae9b975547edd6f0c48602c8deaad03d4
Noel David <noelsdavid@gmail.com>
2017-01-09 11:48:52 +0800

    mlab integration

--- routes/videos.js
+++ routes/videos.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var monk = require('monk');
 var db = monk('mongodb://noel_test:test123@ds157268.mlab.com:57268/vidzy',['videos']);
+// var db = monk('localhost:27017/vidzy');
 
 router.get('/', function(req, res) {
     var collection = db.get('videos');
